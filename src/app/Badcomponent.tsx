8422881af6008983cf7b9eca6603d34532debb15
hoangphuc10x <phuc1092003@gmail.com>
2025-06-22 17:24:55 +0700

    fix: fixing comit-msg

--- src/app/Badcomponent.tsx
+++ src/app/Badcomponent.tsx
@@ -1,7 +1,4 @@
 export default function BadComponent() {
   const msg = 'Hello world'; // lỗi thiếu semicolon
-  const a = 1;
-  //anh chàng đẹp trai ngồi trong quán uống ly cfd
-
-  return <div>{msg}</div>;
+  return <div>{msgửtegwtre}</div>;
 }
