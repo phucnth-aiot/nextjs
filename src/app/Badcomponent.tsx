c70b565cc13231c132653d1c76d7b519a08f9fbd
hoangphuc10x <phuc1092003@gmail.com>
2025-06-22 17:19:14 +0700

    feat: test commitline

--- src/app/Badcomponent.tsx
+++ src/app/Badcomponent.tsx
@@ -1,5 +1,7 @@
 export default function BadComponent() {
   const msg = 'Hello world'; // lỗi thiếu semicolon
   const a = 1;
+  //anh chàng đẹp trai ngồi trong quán uống ly cfd
+
   return <div>{msg}</div>;
 }
