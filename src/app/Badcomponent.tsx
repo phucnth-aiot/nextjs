97f0854fa9426120c9e766a92d98d9e13d6b6270
hoangphuc10x <phuc1092003@gmail.com>
2025-06-22 22:43:42 +0700

    chore: add type check

--- src/app/Badcomponent.tsx
+++ src/app/Badcomponent.tsx
@@ -1,5 +1,6 @@
 export default function BadComponent() {
   const msg = 'Hello world'; // lỗi thiếu semicolon
-  const num: number = "not a number"; // cố tình lỗi
+  const num: number = 'not a number'; // cố tình lỗi
+
   return <div>{msg}</div>;
 }
