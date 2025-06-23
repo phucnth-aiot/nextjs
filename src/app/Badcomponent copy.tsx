c13016705fd382e9c2c662d372693256095d53bf
nguyenthanhhoangphuc109 <phucnth@aiot-global.com>
2025-06-23 11:05:41 +0700

    feat: add page

---
+++ src/app/Badcomponent copy.tsx
@@ -0,0 +1,7 @@
+export default function BadComponent() {
+  const msg = 'Hello world'; // lỗi thiếu semicolon
+
+  const num: number = 'not a number'; // cố tình lỗi
+
+  return <div>{msg}</div>;
+}
