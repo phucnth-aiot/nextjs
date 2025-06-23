cf226150fb3c7a68b53e1e2901b1a05e0c49dba6
nguyenthanhhoangphuc109 <phucnth@aiot-global.com>
2025-06-23 11:25:43 +0700

    fix: fix badcomponent

--- src/app/Badcomponent copy 2.tsx
+++
@@ -1,7 +0,0 @@
-export default function BadComponent() {
-  const msg = 'Hello world'; // lỗi thiếu semicolon
-
-  const num: number = 'not a number'; // cố tình lỗi
-
-  return <div>{msg}</div>;
-}
--- src/app/Badcomponent.tsx
+++ src/app/Badcomponent.tsx
@@ -1,7 +1,4 @@
 export default function BadComponent() {
-  const msg = 'Hello world'; // lỗi thiếu semicolon
-
-  const num: number = 'not a number'; // cố tình lỗi
-
+  const msg = 'Hello world';
   return <div>{msg}</div>;
 }
